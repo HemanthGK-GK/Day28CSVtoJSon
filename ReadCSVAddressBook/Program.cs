10fa905a71db1a3c8ef19170ba1a7db536aec321
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-20 14:34:10 +0530

    Hemanth:Read the data from CSV file

---
+++ ReadCSVAddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadCSVAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            csvHandler.getCSV();
+        }
+    }
+}
