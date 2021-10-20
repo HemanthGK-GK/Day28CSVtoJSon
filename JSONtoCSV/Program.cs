05cd9e31c00e2b030f707b485058114c87aa8285
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-20 14:31:09 +0530

    Hemanth:Reading data from JSON file and Writing into CSV

---
+++ JSONtoCSV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSONtoCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            csvHandler.getCSV();
+        }
+    }
+}
---
+++ JSONtoCSV/csvHandler.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace JSONtoCSV
+{
+    class csvHandler
+    {
+        public static object CsvExport { get; private set; }
+
+        public static void getCSV()
+        {
+            string csvpath = @"C:\Users\91807\Desktop\Batch67\JSONtoCSV\data.csv";
+            string jsonpath = @"C:\Users\91807\Desktop\Batch67\JSONtoCSV\NewFile.json";
+
+            List<DataModel> records = JsonConvert.DeserializeObject<List<DataModel>>(File.ReadAllText(jsonpath));
+
+            Console.WriteLine("Reading from JSON file--------");
+            using (var writer = new StreamWriter(csvpath))
+            using (var CsvWrite = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                CsvWrite.WriteRecords(records);
+            }
+
+        }
+    }
+}
